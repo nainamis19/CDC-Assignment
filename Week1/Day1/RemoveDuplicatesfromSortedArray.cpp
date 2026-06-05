6af3509f4acbaa3190ef674700b9d0932654fa4c
nainamis19 <nayanmis2005@gmail.com>
2026-06-05 20:47:22 +0530

    Create RemoveDuplicatesfromSortedArray.cpp

---
+++ Week1/Day1/RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int removeduplicate(vector<int>&nums){
+    int n=nums.size();
+    if(n==0) return 0;
+    int i=0;
+    for(int j=1;j<n;j++){
+        if(nums[j]!=nums[i]) i++;
+        nums[i]=nums[j];
+    }
+    return i+1;
+}
+int main(){
+        vector<int>nums={0,0,1,1,1,2,2,3,3,4};
+        int ans=removeduplicate(nums);
+        cout<<ans;  
+}
