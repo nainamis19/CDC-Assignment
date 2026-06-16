d1d5736689eda06463f349f3327a9483227741f0
nainamis19 <nayanmis2005@gmail.com>
2026-06-16 16:57:09 +0530

    Create Part2.cpp

---
+++ Week2/OOPS/Part2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Playable {
+public:
+    virtual void play() = 0;
+};
+
+class Saxophone : public Playable {
+public:
+    void play() override {
+        cout << "Play saxophone" << endl;
+    }
+};
